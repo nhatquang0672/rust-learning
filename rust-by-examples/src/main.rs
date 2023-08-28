76a93f49526e704b636ab66ce0980383d8e39617
Quang Tran <nhatquang0672@gmail.com>
2023-08-28 21:26:47 +0700

    structure

--- rust-by-examples/src/main.rs
+++ rust-by-examples/src/main.rs
@@ -1,6 +1,13 @@
-mod tuples;
-use tuples::tuples;
+mod c2_tuples;
+use c2_tuples::tuples;
+
+use crate::c3_structures::c3_structures;
+
+mod c3_structures;
+
 fn main() {
     println!("Hello, World");
-    tuples()
+    // tuples();
+    c3_structures();
+    println!("{}", 3.122*2.333);
 }
