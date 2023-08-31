709b4fe4a7ce4f93d791eaa40adb75ce6c353332
Quang Tran <nhatquang0672@gmail.com>
2023-08-31 09:09:39 +0700

    ownership

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,4 +1,12 @@
 use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let n = vec![1, 2, 3];
+    let y = plus_one(n);
+    println!("The value of y is: {:?}", n);
+}
+
+fn plus_one(y: Vec<i32>) -> i32 {
+    println!("{:?}", y);
+    1
 }
