aa37c861f669850f0fdf63fa4826a51b5f7dc7e8
Quang Tran <nhatquang0672@gmail.com>
2023-12-30 10:45:19 +0700

    update

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,33 +1,5 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-impl Rectangle {    
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
+pub mod enums;
 
-    fn set_width(&mut self, width: u32) {
-        self.width = width;
-    }
-
-    fn max(self, other: Rectangle) -> Rectangle {
-        Rectangle { 
-            width: self.width.max(other.width),
-            height: self.height.max(other.height),
-        }
-    }
-    fn set_to_max(&mut self, other: Rectangle) {
-        let maxx = Rectangle::max(*self, other);
-    }
-}
 fn main() {
-    let scale = 2;
-    let rect1 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
-
-    dbg!(&rect1);
+   println!("abce") ;
 }
