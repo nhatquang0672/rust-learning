965fb8391e450ec8a37fe3b1f611d3713089b6a3
Quang Tran <nhatquang0672@gmail.com>
2024-01-06 20:31:03 +0700

    update

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,37 +1,12 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+use std::rc::Rc;
+use crate::List::{Cons, Nil};
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
 fn main() {
-    let mut list = [
-        Rectangle { width: 10, height: 1 },
-        Rectangle { width: 3, height: 5 },
-        Rectangle { width: 7, height: 12 },
-    ];
-
-    let mut sort_operations = vec![];
-    let value = 1;
-
-    list.sort_by_key(|r| {
-        sort_operations.push(value);
-        r.width
-    });
-    println!("{:#?}", list);
-}
-
-fn iterator_sum() {
-   let v1 = vec![1, 2, 3];
-
-   let v1_iter = v1.iter();
-
-   let total: i32 = v1_iter.sum();
-   dbg!(v1.iter());
-   let v1: Vec<i32> = vec![1, 2, 3];
-
-   let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
-
-   assert_eq!(v2, vec![2, 3, 4]);
-
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
